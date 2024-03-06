e97704b907c0f0fa3bb15e83a78a86b5c3bfa0a3
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-06 09:41:33 +0300

    my favorite books

---
+++ favorite_books.py
@@ -0,0 +1,21 @@
+# Create a tuple containing the names of five of your favorite books. 
+# Then, use a for loop to print each book name on a separate line.
+# print('\n')
+
+# a tuple containing the names  of my favorite books
+print('\n Below are my favorite books! \n')
+
+favorite_books = (
+     "Atomic Habits by James Clear",
+    "The 7 Habits of Highly Effective People by Stephen R. Covey",
+    "Mindset by Carol S. Dweck",
+    "How to Win Friends and Influence People by Dale Carnegie by Dale Carnegie",
+    "Deep Work by Cal Newport"
+)
+
+# loop over each book name in the tuple
+for book in favorite_books:
+    
+    # Print each book name on a separate line
+    print(book, '\n')
+    
